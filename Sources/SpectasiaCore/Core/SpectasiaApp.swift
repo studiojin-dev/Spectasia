76a25e1bef75bdd109e372c1379628a06a5a905d
Kim Jeongjin <kimjj81@gmail.com>
2026-01-27 22:00:07 +0900

    restructure: move core services to Sources/SpectasiaCore/

---
+++ Sources/SpectasiaCore/Core/SpectasiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpectasiaApp.swift
+//  Spectasia
+//
+//  Created by kimjeongjin on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SpectasiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spectasia/SpectasiaTests/SpectasiaTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpectasiaTests.swift
+//  SpectasiaTests
+//
+//  Created by kimjeongjin on 1/27/26.
+//
+
+import Testing
+
+struct SpectasiaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
